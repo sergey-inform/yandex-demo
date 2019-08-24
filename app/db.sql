80f2fffa75a63c43898fa3368912b648382a7043
Sergey Ryzhikov <sergey-inform@ya.ru>
2019-08-25 02:07:24 +0300

    sql cleanup

--- app/db.sql
+++ app/db.sql
@@ -1,18 +1,22 @@
-DROP TABLE IF EXISTS citizen_fields;
+DROP TYPE IF EXISTS citizen_fields;
 DROP TABLE IF EXISTS Relatives CASCADE;
 DROP TABLE IF EXISTS Citizens CASCADE;
 DROP TABLE IF EXISTS Imports;
 DROP SEQUENCE IF EXISTS imports_serial;
 DROP TYPE IF EXISTS gender;
 
+
 CREATE SEQUENCE imports_serial;
+
 
 CREATE TABLE Imports (
 	id   	integer PRIMARY KEY DEFAULT nextval('imports_serial')
 	,shard	varchar
 );
 
+
 CREATE TYPE gender AS ENUM ('female', 'male');
+
 
 CREATE TABLE Citizens (
 	import_id	integer NOT NULL
@@ -26,10 +30,12 @@ CREATE TABLE Citizens (
 	,FOREIGN KEY (import_id) REFERENCES Imports(id) ON DELETE CASCADE
 );
 
+
+/* 1:1 undirected connections */
 CREATE TABLE Relatives (
 	import_id	integer NOT NULL
-	,low      	integer NOT NULL  
-	,high     	integer NOT NULL
+	,low      	integer NOT NULL  /* citizen with lower id */
+	,high     	integer NOT NULL  /* citizen with higher id */
 	
 	,PRIMARY KEY (import_id, low, high)
 	,FOREIGN KEY (import_id) REFERENCES Imports(id) ON DELETE CASCADE
@@ -38,19 +44,23 @@ CREATE TABLE Relatives (
 */
 );
 
-CREATE TABLE citizen_fields(
+
+CREATE TYPE citizen_fields AS (
 	street VARCHAR
 	, building VARCHAR
 	, apartment INT
 	, name VARCHAR
 );
 
+
+/* Json fields -> table columns */
 CREATE OR REPLACE VIEW Citizens_fields AS
 SELECT C.import_id, C.citizen_id, town, to_char(birth_date, 'DD.MM.YYYY') as birth_date, gender, f.* 
 from Citizens as C
 , JSON_POPULATE_RECORD(NULL::citizen_fields, C.fields) f;
 
 
+/* Relatives table -> Arrays of relatives */
 CREATE OR REPLACE VIEW Relatives_agg AS
 SELECT import_id, citizen_id, ARRAY_AGG(rel) as relatives FROM (
 	SELECT import_id, low AS citizen_id, high AS rel
@@ -62,6 +72,7 @@ SELECT import_id, citizen_id, ARRAY_AGG(rel) as relatives FROM (
 GROUP BY sub.import_id, sub.citizen_id;
 
 
+/* Citizens */
 CREATE OR REPLACE VIEW Citizens_view AS
 SELECT CF.*, COALESCE(R.relatives, ARRAY[]::integer[]) as relatives
 FROM Citizens_fields CF
