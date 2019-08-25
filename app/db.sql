8332afd1c8ab437d60741de52c448c1f066778ff
Sergey Ryzhikov <sergey-inform@ya.ru>
2019-08-25 12:43:00 +0300

    birthdays sql

--- app/db.sql
+++ app/db.sql
@@ -20,7 +20,7 @@ CREATE TYPE gender AS ENUM ('female', 'male');
 
 CREATE TABLE Citizens (
 	import_id	integer NOT NULL
-	,citizen_id 	integer NOT NULL
+	,citizen_id integer NOT NULL
 	,town    	varchar	NOT NULL CHECK (town <> '')
 	,birth_date	date	NOT NULL
 	,gender    	gender	NOT NULL
@@ -60,16 +60,20 @@ from Citizens as C
 , JSON_POPULATE_RECORD(NULL::citizen_fields, C.fields) f;
 
 
+/* Relatives in both directions */
+CREATE OR REPLACE VIEW Relatives_all AS
+SELECT import_id, low AS citizen_id, high AS rel
+FROM relatives
+UNION
+SELECT import_id, high AS citizen_id, low AS rel
+FROM relatives;
+
+
 /* Relatives table -> Arrays of relatives */
 CREATE OR REPLACE VIEW Relatives_agg AS
-SELECT import_id, citizen_id, ARRAY_AGG(rel) as relatives FROM (
-	SELECT import_id, low AS citizen_id, high AS rel
-	FROM relatives
-	UNION
-	SELECT import_id, high AS citizen_id, low AS rel
-	FROM relatives
-	) as sub
-GROUP BY sub.import_id, sub.citizen_id;
+SELECT import_id, citizen_id, ARRAY_AGG(rel) as relatives
+FROM Relatives_all as R
+GROUP BY R.import_id, R.citizen_id;
 
 
 /* Citizens */
@@ -80,3 +84,22 @@ LEFT JOIN Relatives_agg AS R
 ON CF.citizen_id = R.citizen_id
 AND CF.import_id = R.import_id;
 
+
+/* Birthdays (we exploit Merge Join here) */
+CREATE OR REPLACE VIEW Birthdays AS
+SELECT R.import_id, C."month", R.citizen_id, COUNT(*) AS presents
+FROM
+  (SELECT import_id, citizen_id, rel
+   FROM Relatives_all
+   ORDER BY rel) AS R,
+
+  (SELECT import_id, citizen_id, date_part('month', birth_date) AS "month"
+   FROM Citizens
+   ORDER BY citizen_id) AS C
+WHERE R.rel = C.citizen_id
+  AND C.import_id = R.import_id
+GROUP BY R.import_id, "month", R.citizen_id
+ORDER BY "month";
+
+/*
+select town, array_agg( ARRAY[age, count]) as age_count from (select town, date_part('year', age( '2019-08-25', birth_date)) as "age", count(*) from  Citizens where import_id = 9  group by town, age order by town, age)as x group by town;*/
